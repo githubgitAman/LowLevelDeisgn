0932216dec7cc7029549e93cdc203278de79eff2
githubgitAman <sharma.aman.a30@gmail.com>
2025-03-24 13:40:39 +0530

    Final Production Level Builder Implementation

--- src/main/java/dev/aman/Builder/Client.java
+++ src/main/java/dev/aman/Builder/Client.java
@@ -2,14 +2,24 @@ package dev.aman.Builder;
 public class Client {
     public static void main(String[] args) {
 
-        Helper helper = Student.getHelper();
-        helper.setName("Aman");
-        helper.setAge(26);
-        helper.setPsp(76.00);
-        helper.setBatch("Jan 2024");
-        helper.setGradYear(2025);
-        helper.setUniversityName("Scaler");
-        Student student = helper.build();
+//        Helper helper = Student.getHelper();
+//        helper.setName("Aman");
+//        helper.setAge(26);
+//        helper.setPsp(76.00);
+//        helper.setBatch("Jan 2024");
+//        helper.setGradYear(2025);
+//        helper.setUniversityName("Scaler");
+//        Student student = helper.build();
+//        System.out.println(student);
+
+        Student student = Student.getHelper()
+                .setName("Aman")
+                .setAge(26)
+                .setPsp(76.00)
+                .setBatch("Jan 2024")
+                .setGradYear(2025)
+                .setUniversityName("Scaler")
+                .build();
         System.out.println(student);
     }
 }
--- src/main/java/dev/aman/Builder/Student.java
+++ src/main/java/dev/aman/Builder/Student.java
@@ -8,7 +8,7 @@ public class Student {
     private int gradYear;
     private String universityName;
 
-    public Student(Helper helper) {
+    private Student(Helper helper) {
         this.name = helper.getName();
         this.age = helper.getAge();
         this.psp = helper.getPsp();
@@ -20,5 +20,61 @@ public class Student {
     public static Helper getHelper(){
         return new Helper();
     }
+    public static class Helper {
+        private String name;
+        private int age;
+        private double psp;
+        private String batch;
+        private int gradYear;
+        private String universityName;
+
+        public String getName(){
+            return name;
+        }
+        public int getAge(){
+            return age;
+        }
+        public double getPsp(){
+            return psp;
+        }
+        public String getBatch(){
+            return batch;
+        }
+        public int getGradYear(){
+            return gradYear;
+        }
+        public String getUniversityName(){
+            return universityName;
+        }
+        public Helper setName(String name){
+            this.name = name;
+            return this;
+        }
+        public Helper setAge(int age){
+            this.age = age;
+            return this;
+        }
+        public Helper setPsp(double psp){
+            this.psp = psp;
+            return this;
+        }
+        public Helper setBatch(String batch){
+            this.batch = batch;
+            return this;
+        }
+        public Helper setGradYear(int gradYear){
+            this.gradYear = gradYear;
+            return this;
+        }
+        public Helper setUniversityName(String universityName){
+            this.universityName = universityName;
+            return this;
+        }
+
+        public Student build(){
+            return new Student(this);
+        }
+    }
+
 }
 
